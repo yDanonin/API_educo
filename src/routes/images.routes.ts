cd48a6ec5a59ea7206563584ac5115b9dd7bd616
Fabiano Barros <fabianorocha102003@gmail.com>
2021-11-10 17:47:24 -0300

    atualização no images.

--- src/routes/images.routes.ts
+++ src/routes/images.routes.ts
@@ -13,11 +13,24 @@ const storage = multer.diskStorage({
     cb(null, 'src/upload')
   },
   filename: function(req, file, cb){
-    cb(null, file.originalname)
+    cb(null, /*new Date().toISOString() +*/ file.originalname)
   }
 })
 
-const upload = multer({ storage: storage })
+/*
+function fileFilter(req, file, cb) {
+  if(file.mimetype === "image/jpg" || file.mimetype === "image/png"){
+    cb(null, true)
+  } else{
+    cb(null, false)
+  }
+}
+*/
+
+const upload = multer({
+  storage: storage//,
+  //fileFilter: fileFilter
+ })
 
 imagesRouter.post('/', upload.single('image'), async (req, res) => {
   try{
